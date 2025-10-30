d1836611e698c239f7c88e009a2af2bec24abe9e
Gabriel Souza <gabrielsouza.74@hotmail.com>
2025-10-30 20:24:16 -0300

    Update base URL in Vite configuration editando o vite
    editando o vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/https://github.com/GabrielSouza74/GabrielSouza/', // 👈 nome exato do seu repositório
+    base: '/GabrielSouza/', // 👈 nome exato do seu repositório
     plugins: [react()],
     optimizeDeps: {
         exclude: ['lucide-react'],
